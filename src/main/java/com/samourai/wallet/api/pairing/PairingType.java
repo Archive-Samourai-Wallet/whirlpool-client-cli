fa7c439e9bd9a953db57644561a5db468833bdb0
zeroleak <zeroleak@samourai.io>
2024-02-08 16:31:12 +0100

    refactor: add support for pairing with samourai.wallet.full 1.0.0

---
+++ src/main/java/com/samourai/wallet/api/pairing/PairingType.java
@@ -0,0 +1,29 @@
+package com.samourai.wallet.api.pairing;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.google.common.base.Optional;
+
+public enum PairingType {
+  WHIRLPOOL_GUI("whirlpool.gui"),
+  SAMOURAI_WALLET_FULL("samourai.wallet.full");
+
+  private String value;
+
+  PairingType(String value) {
+    this.value = value;
+  }
+
+  public static Optional<PairingType> find(String value) {
+    for (PairingType item : PairingType.values()) {
+      if (item.value.equals(value)) {
+        return Optional.of(item);
+      }
+    }
+    return Optional.absent();
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+}
